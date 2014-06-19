9403791ef8a0946cda052ec7a5ffcb37eef28aaa
trippledave <davedannstadt@abwesend.de>
2014-06-19 21:08:29 +0200

    addes service

---
+++ Weblog.Web/Services/IUserService.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Weblog.Core.DataAccess.Weblog;
+using Weblog.Web.Areas.Management.Models;
+using Weblog.Web.Models.Account;
+
+namespace Weblog.Web.Services
+{
+    public interface IUserService
+    {
+        /// <summary>
+        /// Gets the user.
+        /// </summary>
+        /// <param name="userName">Name of the user.</param>
+        /// <returns></returns>
+        UserModel GetUser(string userName);
+
+        /// <summary>
+        /// Gets the user by email.
+        /// </summary>
+        /// <param name="email">The email.</param>
+        /// <returns></returns>
+        UserModel GetUserByEmail(string email);
+
+        /// <summary>
+        /// Creates the user.
+        /// </summary>
+        /// <param name="model">The model.</param>
+        void CreateUser(RegisterModel model);
+
+        /// <summary>
+        /// Checks if the name exists.
+        /// </summary>
+        /// <param name="userName">Name of the user.</param>
+        /// <returns></returns>
+        bool DoesUserNameExist(string userName);
+
+        /// <summary>
+        /// Resets the password.
+        /// </summary>
+        /// <param name="model">The model.</param>
+        void ResetPassword(UserModel model);
+
+        /// <summary>
+        /// Sets the new password.
+        /// </summary>
+        /// <param name="token">The token.</param>
+        /// <param name="password">The password.</param>
+        /// <returns></returns>
+        bool SetNewPassword(string token, string password);
+
+        /// <summary>
+        /// Updates the password.
+        /// </summary>
+        /// <param name="user">The user.</param>
+        /// <param name="currentPassword">The current password.</param>
+        /// <param name="newPassword">The new password.</param>
+        /// <returns></returns>
+        bool UpdatePassword(User user, string currentPassword, string newPassword);
+
+        /// <summary>
+        /// Updates the user settings.
+        /// </summary>
+        /// <param name="model">The model.</param>
+        void UpdateUserSettings(UserSettingsModel model);
+
+    }
+}
---
+++ Weblog.Web/Services/SettingsService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Weblog.Core.DataAccess.Weblog;
+using Weblog.Core.Repositories;
+using Weblog.Web.Models.Management;
+
+namespace Weblog.Web.Services
+{
+    public class SettingsService : ISettingsService
+    {
+
+        private Guid _settingsID = Guid.Empty;
+        private IWeblogRepository _repository = new WeblogRepository();
+        
+        public EmailSettingsModel GetEmailSettings()
+        {
+            return new EmailSettingsModel(_repository.GetAdministratorSettings());
+        }
+
+        public void SetEmailSettings(EmailSettingsModel settings)
+        {
+            AdministratorSettings adminSettings = _repository.GetAdministratorSettings();
+            settings.UpdateSource(adminSettings);
+            _repository.UpdateAdministratorSettings(adminSettings);
+        }
+
+        [Authorize(Roles = "Administrator")]
+        public SiteSettingsModel GetSiteSettings()
+        {
+            return new SiteSettingsModel(_repository.GetAdministratorSettings());
+        }
+
+        public void SetSiteSettings(SiteSettingsModel settings)
+        {
+            AdministratorSettings adminSettings = _repository.GetAdministratorSettings();
+            settings.UpdateSource(adminSettings);
+            _repository.UpdateAdministratorSettings(adminSettings);
+        }
+    }
+}
